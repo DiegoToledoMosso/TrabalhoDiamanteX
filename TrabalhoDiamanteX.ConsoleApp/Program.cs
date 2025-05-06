114957e149398ed9e09f90d1fcd4238d6159513f
Diego Toledo <Diego_toledo3010@hotmail.com>
2025-05-05 23:29:23 -0300

    Versão 1.0
    Indice de tarefas a fazer.

--- TrabalhoDiamanteX.ConsoleApp/Program.cs
+++ TrabalhoDiamanteX.ConsoleApp/Program.cs
@@ -2,6 +2,13 @@
 {
     internal class Program
     {
+        /* Versão 1
+         * O programa deve solicitar ao usuário um número inteiro. x
+         * O número deve ser ímpar. Caso contrário, o programa deve informar que a entrada é inválida e solicitar um novo número. x
+         * O número de linhas e colunas deve ser igual ao número ímpar fornecido.
+         * A linha central do diamante deve conter o número máximo de Xs.
+         */
+
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
